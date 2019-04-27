577e3bf55e28886bc7bacd2a9b6cf6344e8af91a
Smelly Cat <smelly@c.at>
2019-04-28 09:49:27 +1000

    Generate days thrown away, wastefulness, and usefulness

--- src/ptv_fares/core.clj
+++ src/ptv_fares/core.clj
@@ -12,11 +12,30 @@
 (defn- per-day-fare [number-of-days]
   (/ (total-fare number-of-days) (used-days number-of-days)))
 
+(defn- thrown-away [d]
+  (- d (used-days d)))
+
+(defn- wastefulness [d]
+  (float (/ (thrown-away d) d)))
+
+(defn- usefulness [d]
+  (- 1 (wastefulness d)))
+
 (defn- fare-table []
   (->> days
-       (map (fn [d] [d, fare, (total-fare d), (used-days d), (per-day-fare d)]))))
+       (map
+        (fn [d]
+          [d
+           fare
+           (used-days d)
+           (per-day-fare d)
+           (thrown-away d)
+           (wastefulness d)
+           (usefulness d)
+           (total-fare d)]))))
 
-(def header "myki_pass_days, daily_fare, total_fare, used_days, actual_daily_fare")
+(def header
+  "myki_pass_days, daily_fare, used_days, actual_daily_fare, thrown_away, wastefulness, usefulness, total_fare")
 
 (defn -main [& args]
   (doseq [line          (->> (fare-table)
