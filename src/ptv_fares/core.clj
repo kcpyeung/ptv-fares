1ab7910802d0448a684cff5d46d8bbcbbf356dbf
Smelly Cat <smelly@c.at>
2019-04-27 13:01:55 +1000

    Compute days used when Myki Pass bought for in-between weeks

--- src/ptv_fares/core.clj
+++ src/ptv_fares/core.clj
@@ -4,6 +4,19 @@
 
 (def cost-per-day 5.3)
 
-(defn used-days [paid-days]
+(defn- full-weeks [paid-days]
   (->> (/ paid-days 7)
-       (* 5)))
+       int))
+
+(defn- remaining-days-in-partial-week [total-paid-days]
+  (let [paid-days-in-full-weeks (comp (partial * 7) #(full-weeks total-paid-days))]
+    (- total-paid-days (paid-days-in-full-weeks))))
+
+(defn used-days [paid-days]
+  (let [weekdays-in-full-weeks (comp (partial * 5) #(full-weeks paid-days))
+        leftover               (remaining-days-in-partial-week paid-days)
+        is-full-week?          (fn [] (zero? (rem paid-days 7)))]
+    (cond
+      (is-full-week?)             (weekdays-in-full-weeks)
+      :else                       (->> (weekdays-in-full-weeks)
+                                       (+ leftover)))))
--- test/ptv_fares/core_test.clj
+++ test/ptv_fares/core_test.clj
@@ -5,3 +5,11 @@
 (deftest myki-pass-starts-on-monday-and-ends-on-sunday
   (testing "actual days used will be only weekdays"
            (is (= 20 (used-days 28)))))
+
+(deftest myki-pass-starts-on-monday-and-ends-on-weekday
+  (testing "actual days used will be weekdays in the full weeks, plus weekdays in the partial week"
+           (is (= 21 (used-days 29)))
+           (is (= 22 (used-days 30)))
+           (is (= 23 (used-days 31)))
+           (is (= 24 (used-days 32)))
+           (is (= 25 (used-days 33)))))
